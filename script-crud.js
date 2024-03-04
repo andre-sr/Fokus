787b88de50301d2f90fdf420ce534e24cdfdd77c
André S.R <adres.1481@gmail.com>
2024-03-03 23:11:01 -0300

    Inicio do Curso de Local Storage

---
+++ script-crud.js
@@ -0,0 +1,22 @@
+//VARIAVEIS
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
+
+//EVENTOS
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
+
+//FUNÇÕES
